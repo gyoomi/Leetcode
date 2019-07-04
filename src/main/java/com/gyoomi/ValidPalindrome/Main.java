77fcf8b39559f6959e572e93969bcc9d192a983a
gyoomi <gyoomi0709@foxmail.com>
2019-07-04 09:42:48 +0800

    125. 验证回文串 - 双指针

--- src/main/java/com/gyoomi/ValidPalindrome/Main.java
+++ src/main/java/com/gyoomi/ValidPalindrome/Main.java
@@ -53,6 +53,39 @@ class Solution {
     }
 }
 
+class Solution2 {
+
+    /**
+     * 双指针
+     *
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        if (null == s) {
+            return false;
+        }
+        if (s.length() == 0) {
+            return true;
+        }
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
 
 
 
